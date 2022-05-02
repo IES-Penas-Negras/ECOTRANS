56180938b5209ced164d305b02b8a20ee01dd7ae
alumnoeficiente <zufri@hotmail.es>
2022-05-02 13:54:06 +0200

    Creación de carpeta objetos & acciones

---
+++ Ecotrans/Nucleo/Acciones/Objeto/AccionesObjeto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nucleo.Acciones.Objeto
+{
+    internal class AccionesObjeto
+    {
+        public void Crear() { }
+        public void Listar() { }
+        public void Editar() { }
+        public void Borrar() { }
+
+    }
+}
