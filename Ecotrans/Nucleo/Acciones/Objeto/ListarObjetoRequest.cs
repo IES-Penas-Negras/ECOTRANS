20a96eac6f68ffa6c73971494263e46e32a33ed1
merienditas <maricersal@hotmail.com>
2022-05-03 11:02:42 +0200

    clases hechas, en proceso aun

---
+++ Ecotrans/Nucleo/Acciones/Objeto/ListarObjetoRequest.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IESPeñasNegras.Ecotrans.Nucleo.Acciones.Objeto;
+
+public class ListarObjetoRequest 
+{
+
+}
