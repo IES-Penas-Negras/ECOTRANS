31df65cb87a82aecee87d4201049f745f0cc9a39
Tai55 <jesus.393hernandez@gmail.com>
2022-05-03 09:18:11 +0200

    Arreglado BBDD

--- Ecotrans/Nucleo/BBDD/DonacionesContext.cs
+++ Ecotrans/Nucleo/BBDD/DonacionesContext.cs
@@ -5,7 +5,7 @@ namespace Ecotrans.Nucleo.BBDD
 {
 	public class DonacionesContext : DbContext
 	{
-		public string ConnectionString = "Server=Localhost;Database=Vidly;Trusted_Connection=True;";
+		public string ConnectionString = "Server=Localhost;Database=Ecotrans;Trusted_Connection=True;";
 		protected override void OnConfiguring(DbContextOptionsBuilder options)
         => options.UseSqlServer(ConnectionString);
 	}
