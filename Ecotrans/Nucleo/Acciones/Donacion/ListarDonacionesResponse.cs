3464621c85de65e094c66932274b6b2de31b8f62
Mona_Lisa <monalisa@davinci.com>
2022-05-05 10:51:24 +0200

    Terminado Issue #17

---
+++ Ecotrans/Nucleo/Acciones/Donacion/ListarDonacionesResponse.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IESPeñasNegras.Ecotrans.Nucleo.Acciones.Donacion
+{
+    public class ListarDonacionesResponse
+    {
+        public List<ListarDonacionesElemento> Elementos { get; set; }
+    }
+
+    public class ListarDonacionesElemento
+    {
+       
+        public string Nombre { get; set; }
+        public int TipoObjetoID { get; set; }
+        public Estado Estado { get; set;}
+        public decimal Altura { get; set; }
+        public decimal Anchura { get; set; }
+        public decimal Profundidad { get; set; }
+        public decimal Peso { get; set; }
+
+    }
+}
+    
