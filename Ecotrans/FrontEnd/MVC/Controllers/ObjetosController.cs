69971d563f74db61d177e673d170d3c275f3a309
Tai55 <jesus.393hernandez@gmail.com>
2022-05-05 11:08:31 +0200

    EN proceso

---
+++ Ecotrans/FrontEnd/MVC/Controllers/ObjetosController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using IESPeñasNegras.Ecotrans.Nucleo.Acciones.Objeto;
+
+namespace IESPeniasNegras.Ecotrans.MVC.Controllers;
+
+public class ObjetosController : Controller
+{
+    public IActionResult Index()
+    {
+
+    }
+}
