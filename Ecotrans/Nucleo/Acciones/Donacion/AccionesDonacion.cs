3e1a905ff60ca1b48499451dc03b7d657822e854
Angelill014 <angelsanchezdelacruz@gmail.com>
2022-05-05 11:09:18 +0200

    Crear clase editar reponse y donacion

--- Ecotrans/Nucleo/Acciones/Donacion/AccionesDonacion.cs
+++ Ecotrans/Nucleo/Acciones/Donacion/AccionesDonacion.cs
@@ -15,20 +15,7 @@ public class AccionesDonacion
     }
     
     public void Borrar()
-    {}
+    {
 
-public class request
-{
-    public string Usuario {get; set;}
-    public string Titulo {get; set;}
-    public DateTime FechaInicio {get; set;}
-    public DateTime? FechaFin {get; set;}
-    public string Descripcion {get; set;}
-}
-
-public class reponse
-{
-
-}
-
+    }
 }
