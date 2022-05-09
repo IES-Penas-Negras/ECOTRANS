294039ed1e53f3bd4ece9e9071d11b23bc30c209
Fatima <fatimabha99@gmail.com>
2022-05-09 14:20:38 +0200

    Crear controller para las donaciones

---
+++ Ecotrans/FrontEnd/MVC/Controllers/DonacionesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC.Controllers
+{
+    public class DonacionesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        
+    }
+
+}
