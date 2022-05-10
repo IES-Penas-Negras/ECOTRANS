f293da92ce39105612349f428808cc7b2779df6f
merienditas <maricersal@hotmail.com>
2022-05-10 10:58:28 +0200

    #20 Creamos migracion de entidad objeto

--- Ecotrans/Nucleo/Migrations/20220510085746_CrearEntidadObjeto.Designer.cs
+++ Ecotrans/Nucleo/Migrations/20220510085746_CrearEntidadObjeto.Designer.cs
@@ -3,6 +3,7 @@ using Ecotrans.Nucleo.BBDD;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Nucleo.Migrations
 {
     [DbContext(typeof(DonacionesContext))]
-    partial class DonacionesContextModelSnapshot : ModelSnapshot
+    [Migration("20220510085746_CrearEntidadObjeto")]
+    partial class CrearEntidadObjeto
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Ecotrans/Nucleo/Migrations/20220510085746_CrearEntidadObjeto.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Nucleo.Migrations
+{
+    public partial class CrearEntidadObjeto : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TipoObjeto",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TipoObjeto", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Objetos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(300)", maxLength: 300, nullable: false),
+                    TipoObjetoId = table.Column<int>(type: "int", nullable: false),
+                    Estado = table.Column<int>(type: "int", nullable: false),
+                    Altura = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Anchura = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Profundidad = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Peso = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Objetos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Objetos_TipoObjeto_TipoObjetoId",
+                        column: x => x.TipoObjetoId,
+                        principalTable: "TipoObjeto",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Objetos_TipoObjetoId",
+                table: "Objetos",
+                column: "TipoObjetoId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Objetos");
+
+            migrationBuilder.DropTable(
+                name: "TipoObjeto");
+        }
+    }
+}
