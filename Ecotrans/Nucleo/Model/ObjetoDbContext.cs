0e2d4176ebaafabb1b20d389213f41f3c5d30672
merienditas <maricersal@hotmail.com>
2022-05-10 10:24:56 +0200

    #20 añadiendo DbSet

---
+++ Ecotrans/Nucleo/Model/ObjetoDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace IESPeñasNegras.Ecotrans.Modelo.Objeto.DbContext
+{
+	public class ObjetoDbContext : DbContext
+	{
+
+	public DbSet <Objeto> Objeto { get; set; }
+	
+	}
+
+
+} 
+
+	
