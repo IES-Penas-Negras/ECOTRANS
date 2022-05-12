1e75b2b17f48a869da9d109066e5bcbe9b11d987
Tai55 <jesus.393hernandez@gmail.com>
2022-05-12 10:25:30 +0200

    commit

--- Ecotrans/FrontEnd/MVC/Controllers/ObjetosController.cs
+++ Ecotrans/FrontEnd/MVC/Controllers/ObjetosController.cs
@@ -1,16 +1,13 @@
 using IESPeniasNegras.Ecotrans.Nucleo.AccionesObjeto;
 using Microsoft.AspNetCore.Mvc;
 
-
 namespace IESPeniasNegras.Ecotrans.MVC.Controllers;
 
 public class ObjetosController : Controller
 {
     public IActionResult Index()
     {
-       var objeto = new AccionesObjeto()
-        return View(objeto);
-        
+        var objeto = new AccionesObjeto()
+        return View(objeto);  
     }
-    
 }
