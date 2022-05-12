5fc74e204e7f6f666e790c88c53eff03cb20cb2f
alumnoeficiente <zufri@hotmail.es>
2022-05-12 11:00:55 +0200

    Añadido el contexto de la Base de datos a las acciones de objeto.

--- Ecotrans/Nucleo/Acciones/Objeto/AccionesObjeto.cs
+++ Ecotrans/Nucleo/Acciones/Objeto/AccionesObjeto.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Ecotrans.Nucleo.BBDD;
+using IESPeniasNegras.Ecotrans.Nucleo.Acciones.Objeto;
 
 namespace IESPeniasNegras.Ecotrans.Nucleo.Acciones.Objeto
 {
     public class AccionesObjeto
     {
+        private readonly DonacionesContext contexto;
+
+        public AccionesObjeto()
+        {
+            contexto = new DonacionesContext();
+        }
+
         public CrearObjetoResponse Crear(CrearObjetoRequest crearObjetoRequest) 
         {
             return new CrearObjetoResponse();
