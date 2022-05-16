6e5761910ce72902a3e05affdb7b1567eac4e3b1
Fatima <fatimabha99@gmail.com>
2022-05-16 14:14:40 +0200

    Añadir AutoMapper
    #29

---
+++ Ecotrans/Nucleo/Acciones/Donacion/DonacionesProfile.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Modelo = IESPeniasNegras.Ecotrans.Nucleo.Model;
+
+namespace IESPeniasNegras.Ecotrans.Nucleo.Acciones.Donacion
+{
+    public class DonacionesProfile : Profile
+    {
+        public DonacionesProfile()
+        {
+            CreateMap<CrearDonacionRequest, Modelo.Donacion>();
+            CreateMap<Modelo.Donacion, CrearDonacionResponse>();
+
+            CreateMap<EditarDonacionRequest, Modelo.Donacion>();
+            CreateMap<Modelo.Donacion, EditarDonacionResponse>();
+
+            CreateMap<ListarDonacionRequest, Modelo.Donacion>();
+            CreateMap<Modelo.Donacion, ListarDonacionResponse>();
+
+            CreateMap<BorrarDonacionRequest, Modelo.Donacion>();
+        }
+    }
+}
