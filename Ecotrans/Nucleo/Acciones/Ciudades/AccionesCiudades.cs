38fc8168c4d85749a821f45157bc3626ee0d9029
Mona_Lisa <monalisa@davinci.com>
2022-05-16 13:34:15 +0200

    Crear Acciones Ciudades

---
+++ Ecotrans/Nucleo/Acciones/Ciudades/AccionesCiudades.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IESPeniasNegras.Ecotrans.Nucleo.Acciones.Ciudades;
+
+namespace IESPeniasNegras.Ecotrans.Nucleo.Acciones.Ciudades
+{
+    public class AccionesCiudades
+    {
+        public void Listar()
+        {
+        }
+        public void Crear()
+        { 
+        }
+
+      
+
+        public void Editar()
+        {
+
+        }
+
+        public void Borrar()
+        {
+
+        }
+    }
+}
+
