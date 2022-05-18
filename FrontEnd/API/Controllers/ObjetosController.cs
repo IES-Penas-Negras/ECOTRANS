97ac498c571984626111f1b7c5e406457e86d398
alumnoeficiente <zufri@hotmail.es>
2022-05-18 14:05:36 +0200

    Issue #35 Creado el Controller de Objetos en API

---
+++ FrontEnd/API/Controllers/ObjetosController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ObjetosController : ControllerBase
+    {
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<ValuesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+        //[HttpGet("{id}")]
+        //public ActionResult <>
+        // POST api/<ValuesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ValuesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ValuesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
