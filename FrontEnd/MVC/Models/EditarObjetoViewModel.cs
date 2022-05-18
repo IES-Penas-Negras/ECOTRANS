dc38b4a9b0758b4924e57673cdc8d8a28102d789
Fatima <fatimabha99@gmail.com>
2022-05-18 14:08:57 +0200

    Creando vista edición de objetos
    #34

---
+++ FrontEnd/MVC/Models/EditarObjetoViewModel.cs
@@ -0,0 +1,12 @@
+﻿using IESPeniasNegras.Ecotrans.Nucleo.Acciones.Objeto;
+using IESPeniasNegras.Ecotrans.Nucleo.Model;
+
+namespace IESPeniasNegras.Ecotrans.MVC.Models
+{
+    public class EditarObjetoViewModel : EditarObjetoRequest
+    {
+       public TipoObjeto TipoObjeto { get; set; }
+       public IEnumerable<Estado> Estado { get; set; }
+
+    }
+}
