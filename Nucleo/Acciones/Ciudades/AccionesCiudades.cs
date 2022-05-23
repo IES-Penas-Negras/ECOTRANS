b2c646c6f0144ed27197ef31584a96b9f9d2e889
Mona_Lisa <monalisa@davinci.com>
2022-05-23 13:14:42 +0200

    .

--- Nucleo/Acciones/Ciudades/AccionesCiudades.cs
+++ Nucleo/Acciones/Ciudades/AccionesCiudades.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Ecotrans.Nucleo.BBDD;
+using IESPeniasNegras.Ecotrans.Nucleo.BBDD;
 using IESPeniasNegras.Ecotrans.Nucleo.Acciones.Ciudades;
 
 namespace IESPeniasNegras.Ecotrans.Nucleo.Acciones.Ciudades
