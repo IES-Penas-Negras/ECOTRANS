592be6d7e8a166a2609e6a594956841d91625a42
Mona_Lisa <monalisa@davinci.com>
2022-05-23 13:41:12 +0200

    Última actualización

--- FrontEnd/API/Controllers/DonacionesController.cs
+++ FrontEnd/API/Controllers/DonacionesController.cs
@@ -6,9 +6,36 @@ namespace API.Controllers
     [ApiController]
     public class DonacionesController : ControllerBase
     {
-            public IActionResult Index()
-            {
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
 
-            }
+        // GET api/<ValuesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ValuesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ValuesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ValuesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
     }
 }
