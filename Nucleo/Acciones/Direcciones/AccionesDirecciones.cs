8d9e3cd502ac3bb1dd9aa5c799069c3ca21e12b5
Fatima <fatimabha99@gmail.com>
2022-05-23 14:15:28 +0200

    Creando acciones direcciones y crear contexto base de datos.
    #41

---
+++ Nucleo/Acciones/Direcciones/AccionesDirecciones.cs
@@ -0,0 +1,44 @@
+﻿using IESPeniasNegras.Ecotrans.Nucleo.BBDD;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IESPeniasNegras.Ecotrans.Nucleo.Acciones.Direcciones
+{
+    public class AccionesDirecciones : IDisposable
+    {
+        private DonacionesContext contexto;
+        public AccionesDirecciones()
+        {
+            contexto = new DonacionesContext();
+        }
+        public void Dispose()
+        {
+            contexto.Dispose();   
+        }
+        public CrearDireccionResponse Crear(CrearDireccionRequest crearDireccionRequest)
+        {
+            var crearDireccion = contexto.Direcciones;
+            return new CrearDireccionResponse();
+        }
+
+        public EditarDireccionResponse Editar(EditarDireccionRequest editarDireccionRequest)
+        {
+            var editarDireccion = contexto.Direcciones;
+            return new EditarDireccionResponse();
+        }
+
+        public ListarDireccionResponse Listar(ListarDireccionRequest listarDireccionRequest, int id)
+        {
+            var listarDireccion = contexto.Direcciones.SingleOrDefault(d => d.Id == id);
+            return new ListarDireccionResponse();
+        }
+
+        public void Borrar(BorrarDireccionRequest borrarDireccionRequest)
+        {
+            var borrarDireccion = contexto.Direcciones;
+        }
+    }
+}
