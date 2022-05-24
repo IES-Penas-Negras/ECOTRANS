04a85a54b52f83d4843d50c542f0653994067023
Tai55 <jesus.393hernandez@gmail.com>
2022-05-24 09:49:04 +0200

    cambios leves

--- Test/DonacionesTests.cs
+++ Test/DonacionesTests.cs
@@ -1,18 +1,21 @@
 using Xunit;
 using IESPeniasNegras.Ecotrans.Nucleo.Acciones.Objeto;
 using IESPeniasNegras.Ecotrans.Nucleo.BBDD;
+namespace IESPeniasNegras.Ecotrans.Test;
 
-
-
-namespace IESPeniasNegras.Ecotrans.Test
+public class DonacionesObjeto
 {
-    public class DonacionesTest
+    public DonacionesObjeto()
     {
-        public DonacionesTest()
+        public DonacionesObjeto(DonacionesContext? donacionesContext = null, IMapper? mapper = null)
         {
-            public AccionesDonacion(DonacionesTestContext? don)
-        }
-
+            if (donacionesContext == null)
+            {
+                contexto = new DonacionesContext();
+            }
+            else
+            {
+                contexto = donacionesContext;
+            }
     }
 }
-
