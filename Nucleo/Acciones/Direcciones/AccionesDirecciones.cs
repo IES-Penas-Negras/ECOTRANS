2b9527941190082d42f47e02e5d5c6a19559aa37
Fatima <fatimabha99@gmail.com>
2022-05-24 11:09:12 +0200

    Creada clase direccion profile y creando contexto para crear, editar, listar y borrar.
    #41

--- Nucleo/Acciones/Direcciones/AccionesDirecciones.cs
+++ Nucleo/Acciones/Direcciones/AccionesDirecciones.cs
@@ -1,44 +1,86 @@
-﻿using IESPeniasNegras.Ecotrans.Nucleo.BBDD;
+﻿using AutoMapper;
+using IESPeniasNegras.Ecotrans.Nucleo.BBDD;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Modelo = IESPeniasNegras.Ecotrans.Nucleo.Model;
 
 namespace IESPeniasNegras.Ecotrans.Nucleo.Acciones.Direcciones
 {
     public class AccionesDirecciones : IDisposable
     {
-        private DonacionesContext contexto;
-        public AccionesDirecciones()
+        private readonly DonacionesContext contexto;
+        private readonly IMapper mapper;
+        public AccionesDirecciones(DonacionesContext? donacionesContext = null, IMapper? mapper = null)
         {
-            contexto = new DonacionesContext();
+            if (donacionesContext == null)
+            {
+                contexto = new DonacionesContext();
+            }
+            else
+            {
+                contexto = donacionesContext;
+            }
+
+            this.mapper = mapper ?? new MapperConfiguration(cfg => cfg.AddProfile<DireccionesProfile>()).CreateMapper();
         }
         public void Dispose()
         {
             contexto.Dispose();   
         }
+
         public CrearDireccionResponse Crear(CrearDireccionRequest crearDireccionRequest)
         {
-            var crearDireccion = contexto.Direcciones;
-            return new CrearDireccionResponse();
+            var crearDireccion = mapper.Map<Modelo.Direccion>(crearDireccionRequest);
+
+            contexto.Direcciones.Add(crearDireccion);
+            contexto.SaveChanges();
+
+            return mapper.Map<CrearDireccionResponse>(crearDireccion);
         }
 
         public EditarDireccionResponse Editar(EditarDireccionRequest editarDireccionRequest)
         {
-            var editarDireccion = contexto.Direcciones;
-            return new EditarDireccionResponse();
+            var editarDireccion = mapper.Map<Modelo.Direccion>(editarDireccionRequest);
+
+            if (editarDireccion == null)
+                contexto.Direcciones.Add(editarDireccion);
+
+            else
+            {
+                var direccion = contexto.Direcciones.Single(d => d.Id == editarDireccion.Id);
+                
+                direccion.ProvinciaId = editarDireccion.ProvinciaId;
+                direccion.CiudadId = editarDireccion.CiudadId;
+                direccion.Direccion1 = editarDireccion.Direccion1;
+                direccion.Direccion2 = editarDireccion.Direccion2;
+                direccion.CodigoPostal = editarDireccion.CodigoPostal;
+
+                contexto.SaveChanges();
+            }
+
+            return mapper.Map<EditarDireccionResponse>(editarDireccion);
         }
 
         public ListarDireccionResponse Listar(ListarDireccionRequest listarDireccionRequest, int id)
         {
-            var listarDireccion = contexto.Direcciones.SingleOrDefault(d => d.Id == id);
-            return new ListarDireccionResponse();
+            var listarDireccion = mapper.Map<Modelo.Direccion>(listarDireccionRequest);
+
+            contexto.Direcciones.SingleOrDefault(d => d.Id == id);
+
+            return mapper.Map<ListarDireccionResponse>(listarDireccion);
         }
 
         public void Borrar(BorrarDireccionRequest borrarDireccionRequest)
         {
-            var borrarDireccion = contexto.Direcciones;
+            var borrarDireccion = mapper.Map<Modelo.Direccion>(borrarDireccionRequest);
+
+            contexto.Direcciones.Single(d => d.Id == borrarDireccion.Id);
+            contexto.Direcciones.Remove(borrarDireccion);
+            contexto.SaveChanges();
         }
     }
 }
