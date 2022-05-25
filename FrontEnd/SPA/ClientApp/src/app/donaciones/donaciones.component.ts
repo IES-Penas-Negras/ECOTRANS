2daa1d46961fb8fe07bd9b9c49e72aac4d3446f3
Carlos Pareja <saulroc@hotmail.com>
2022-05-25 12:17:58 +0200

    Añadido proyecto SPA con angular

---
+++ FrontEnd/SPA/ClientApp/src/app/donaciones/donaciones.component.ts
@@ -0,0 +1,27 @@
+import { Component, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-donaciones',
+  templateUrl: './donaciones.component.html'
+})
+export class DonacionesComponent {
+  public donaciones: ListarDonacionElemento[] = [];
+
+  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+    http.get<ListarDonacionResponse>(baseUrl + 'donaciones').subscribe(result => {
+      this.donaciones = result.elementos;
+    }, error => console.error(error));
+  }
+}
+
+interface ListarDonacionResponse {
+    elementos: ListarDonacionElemento[];
+}
+
+interface ListarDonacionElemento {
+  date: string;
+  temperatureC: number;
+  temperatureF: number;
+  summary: string;
+}
