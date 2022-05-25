40d34faad489e77e85979cb1c3e65513bf34deb0
Tai55 <jesus.393hernandez@gmail.com>
2022-05-25 14:04:14 +0200

    test aun sin terminar

--- Test/ObjetosTest.cs
+++ Test/ObjetosTest.cs
@@ -1,10 +1,11 @@
 using Xunit;
+using System;
 using System.Linq;
 using IESPeniasNegras.Ecotrans.Nucleo.Acciones.Objeto;
 using IESPeniasNegras.Ecotrans.Nucleo.Acciones.Donacion;
-using IESPeniasNegras.Ecotrans.Nucleo.BBDD;
 using IESPeniasNegras.Ecotrans.Nucleo.Model;
-using IESPeniasNegras.Ecotrans;
+using IESPeniasNegras.Ecotrans.Test;
+x
 
 public class ObjetosTest
 {
@@ -16,24 +17,40 @@ public class ObjetosTest
         accionesObjeto = new AccionesObjeto(contexto); 
     }
     [Fact]
-    public void Debe_Crear_Una_Donacion()
+    public void Debe_Crear_Una_Donacion(AccionesDonacion accionesDonacion)
     {
         //Given
         var donacion = new Donacion()
         {
-            Nombre = "Silla",
-            FechaInicio = "25/05/2022",
-            FechaFin = "30/06/2022",
+            Titulo = "",
+            /*FechaInicio = "25/05/2022",
+            FechaFin = "30/06/2022",*/
             Descripcion = "Silla en muy buen estado, fabricada en madera de roble, perfecta para poner en el salon"
         };
         //When
         var peticionDonacion = new CrearDonacionRequest()
         {
-            Nombre = donacion.Nombre,
+            Titulo = donacion.Titulo,
             FechaInicio = donacion.FechaInicio,
             FechaFin = donacion.FechaFin,
             Descripcion = donacion.Descripcion
         };
+        var respuestaDonacion = accionesDonacion.Crear(peticionDonacion);
+        //Then
+        Assert.Equal(respuestaDonacion.Titulo, donacion.Titulo);
+        Assert.Equal(respuestaDonacion.FechaInicio, donacion.FechaInicio);
+        Assert.Equal(respuestaDonacion.FechaFin , donacion.FechaFin);
+        Assert.Equal(respuestaDonacion.Descripcion, donacion.Descripcion);
     }
+    [Fact]
+    public void Debe_Editar_Una_Donacion_Existente()
+    {
+        //Given
+        var donacion = new Donacion()
+        {
+            
+        };
+    }
+
 }
 
