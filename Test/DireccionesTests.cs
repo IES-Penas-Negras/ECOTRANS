27facce2642d6f40ab5489028a6b04cfab559b27
alumnoeficiente <zufri@hotmail.es>
2022-05-30 12:22:03 +0200

    Issue #54 Creado "DireccionesTests"

---
+++ Test/DireccionesTests.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using IESPeniasNegras.Ecotrans.Nucleo.Acciones;
+using IESPeniasNegras.Ecotrans.Nucleo.Model;
+using Xunit;
+
+namespace IESPeniasNegras.Ecotrans.Test;
+public class DireccionesTest
+{
+    private readonly DonacionesTestContext contexto;
+    private readonly AccionesDireccion accionesDireccion;
+
+    public AccionesTest()
+    {
+        contexto = new DonacionesTestContext();
+        accionesDireccion = new AccionesDireccion(contexto);
+    }
+}
+
