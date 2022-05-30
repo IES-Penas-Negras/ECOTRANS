011a1f87ac21281edee989d55ecacd18180cfb60
Tai55 <jesus.393hernandez@gmail.com>
2022-05-30 14:05:43 +0200

    Terminar en casa #38

--- Test/DonacionesTest.cs
+++ Test/DonacionesTest.cs
@@ -6,11 +6,11 @@ using IESPeniasNegras.Ecotrans.Nucleo.Acciones.Donacion;
 using IESPeniasNegras.Ecotrans.Nucleo.Model;
 using IESPeniasNegras.Ecotrans.Test;
 
-public class ObjetosTest
+public class DonacionesTest
 {
     private readonly DonacionesTestContext contexto;
     private readonly AccionesDonacion accionesDonacion;
-    public ObjetosTest()
+    public DonacionesTest()
     {
         contexto = new DonacionesTestContext();         
         accionesDonacion = new AccionesDonacion(); 
@@ -27,21 +27,42 @@ public class ObjetosTest
             Descripcion = "Silla en muy buen estado, fabricada en madera de roble, perfecta para poner en el salon"
         };
         //When
-        var peticionDonacion = new CrearDonacionRequest()
+        var crearDonacion = new CrearDonacionRequest()
         {
             Titulo = donacion.Titulo,
             FechaInicio = donacion.FechaInicio,
             FechaFin = donacion.FechaFin,
             Descripcion = donacion.Descripcion
         };
-        var peticionCrearDonacion = new ListarDonacionRequest();
-        var respuestaDonacion = new AccionesDonacion.Crear(peticionCrearDonacion);
+        var peticion = new ListarDonacionRequest();
+        var respuestaDonacion = new AccionesDonacion.Crear(peticion);
+        
         //Then
         Assert.Equal(respuestaDonacion.Titulo, donacion.Titulo);
         Assert.Equal(respuestaDonacion.FechaInicio, donacion.FechaInicio);
         Assert.Equal(respuestaDonacion.FechaFin , donacion.FechaFin);
         Assert.Equal(respuestaDonacion.Descripcion, donacion.Descripcion);
     }
+    [Fact]
+    public void Debe_Listar_Una_Donacion_Existente()
+    {
+        //Given
+        var donacion = new Donacion()
+        {
+            Titulo = "",
+            /*FechaInicio = ,
+            FechaFin = ,*/
+             Descripcion = "Silla en muy buen estado, fabricada en madera de roble, perfecta para poner en el salon"
+        };
+        contexto.Donaciones.Add(donacion);
+        contexto.SaveChanges();
+        //When
+        var peticion = new ListarDonacionRequest();
+        var respuesta = accionesDonacion.Listar(peticion);
+        //Then
+        Assert.NotEmpty(respuesta.Elementos);
+    }
+        
     [Fact]
     public void Debe_Editar_Una_Donacion_Existente()
     {
@@ -55,8 +76,8 @@ public class ObjetosTest
         };
         
         //When
-        var peticionDonacion = new ListarDonacionRequest();
-        var respuestaDonacion = AccionesDonacion.Listar(peticionDonacion);
+        var peticion = new ListarDonacionRequest();
+        var respuestaDonacion = AccionesDonacion.Listar(peticion);
         //Then
         Assert.NotEmpty(respuestaDonacion.Elementos);
     }
@@ -79,7 +100,7 @@ public class ObjetosTest
         {
             Buscar= "Silla"
         };
-        var respuesta= accionesDonacion.Listar(peticion);
+        var respuesta= AccionesDonacion.Listar(peticion);
         // Then
         Assert.Empty(respuesta.Elementos);
         
@@ -98,26 +119,19 @@ public class ObjetosTest
         //When
         var peticion = new EditarDonacionRequest()
         {
-            /*IdEdicion = donacion.Id,
-            Nombre = "Mesa",
-            TipoDonacionId = donacion.TipoDonacionId,
-            Estado = donacion.Estado,
-            Altura = donacion.Altura,
-            Anchura = donacion.Anchura,
-            Profuncidad = donacion.Profuncidad,
-            Peso = donacion.Peso
-            */
+            IdEdicion = donacion.Id,
+            Titulo = donacion.Titulo,
+            FechaInicio = donacion.FechaInicio,
+            FechaFin = donacion.FechaFin,
+            Descripcion = donacion.Descripcion
         };
         var respuesta = accionesDonacion.Editar(peticion);
         //Then
-        /*
         Assert.Equal(respuesta.Id, donacion.Id);
-        Assert.Equal(respuesta.Estado, donacion.Estado);
-        Assert.Equal(respuesta.Alutura, donacion.Altura);
-        Assert.Equal(respuesta.Anchura, donacion.Anchura);
-        Assert.Equal(respuesta.Profundida, donacion.Profundidad);
-        Assert.Equal(respuesta.Perso, donacion.Peso);
-        */
+        Assert.Equal(respuesta.Titulo, donacion.Titulo);
+        Assert.Equal(respuesta.FechaInicio, donacion.FechaInicio);
+        Assert.Equal(respuesta.FechaFin, donacion.FechaFin);
+        Assert.Equal(respuesta.Descripcion, donacion.Descripcion);
     }
     [Fact]
     public void Debe_Borrar_Una_Donacion_Existente()
