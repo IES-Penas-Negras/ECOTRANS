a5065f5c557d6dd67f65e784044cfb0ad2e24c36
Fatima <fatimabha99@gmail.com>
2022-06-01 12:45:38 +0200

    Creadas acciones para tipo objeto
    #55

---
+++ Nucleo/Acciones/TipoObjeto/AccionesTipoObjeto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IESPeniasNegras.Ecotrans.Nucleo.Acciones.TipoObjeto
+{
+    public class AccionesTipoObjeto
+    {
+        public CrearTipoObjetoResponse Crear(CrearTipoObjetoRequest crearTipoObjetoRequest)
+        {
+            return new CrearTipoObjetoResponse();
+        }
+
+        public EditarTipoObjetoResponse Editar(EditarTipoObjetoRequest editarTipoObjetoRequest)
+        {
+            return new EditarTipoObjetoResponse();
+
+        }
+
+        public ListarTipoObjetoResponse Listar(ListarTipoObjetoRequest listarTipoObjetoRequest)
+        {
+            return new ListarTipoObjetoResponse();
+        }
+
+        public void Borrar(BorrarTipoObjetoRequest borrarTipoObjetoRequest)
+        {
+            
+        }
+
+    }
+}
