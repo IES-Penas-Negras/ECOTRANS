7354be820ac5711ec5b32a3ca8298cbdf7bca885
Mona_Lisa <monalisa@davinci.com>
2022-06-01 13:37:00 +0200

    Acciones Creadas

--- FrontEnd/MVC/Controllers/ObjetosController.cs
+++ FrontEnd/MVC/Controllers/ObjetosController.cs
@@ -6,11 +6,16 @@ namespace IESPeniasNegras.Ecotrans.MVC.Controllers;
 public class ObjetosController : Controller
 {
 
+    public AccionesObjeto AccionesObjeto { get; set; }
+
+    public ObjetosController(AccionesObjeto AccionesObjeto)
+    {
+        this.AccionesObjeto = AccionesObjeto;
+    }
+
     public IActionResult Index(string? buscar = null)
     {
-        var objeto = new AccionesObjeto();
-        var request = new ListarObjetoRequest(buscar);
-        return View(objeto.Listar(request));  
-
+        var elementos = AccionesObjeto.Listar(new ListarObjetoRequest(buscar));
+        return View(elementos);
     }
 }
