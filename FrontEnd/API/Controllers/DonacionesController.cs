66a646b244659bdb1f81b65ea106b363a16dcec4
Mona_Lisa <monalisa@davinci.com>
2022-06-01 12:44:21 +0200

    Merge DonacionesController

--- FrontEnd/API/Controllers/DonacionesController.cs
+++ FrontEnd/API/Controllers/DonacionesController.cs
@@ -27,42 +27,33 @@ namespace IESPeniasNegras.Ecotrans.API.Controllers
         [HttpGet("{id}")]
         public ListarDonacionResponse Get(string? buscar)
         {
-            var listarRequest = new ListarDonacionRequest(buscar);
+            var listarDonacionRequest = new ListarDonacionRequest(buscar);
             return accionesDonacion.Listar(listarRequest);
         }
 
         [HttpPost]
         public ActionResult<CrearDonacionResponse> CrearDonacion ([FromBody] CrearDonacionRequest donacion)
-        public ActionResult<CrearDonacionResponse> Crear([FromBody] CrearDonacionRequest donacion)
         {
             if (!ModelState.IsValid)
                 return BadRequest(donacion);
             var creacionDonacion = accionesDonacion.Crear(donacion);
             return Created(Request.Path.Value + creacionDonacion.Id, creacionDonacion);
-            if (!ModelState.IsValid)
-                return BadRequest(donacion);
-            var creacionDonacion = accionesDonacion.Crear(donacion);
-             return Created(Request.Path.Value + creacionDonacion.Id, creacionDonacion);
+            
         }
        
 
         [HttpPut("{id}")]
         public ActionResult<EditarDonacionResponse> EditarDonacion(int id, [FromBody] EditarDonacionRequest donacion)
-        public ActionResult<EditarDonacionResponse> Editar (int id, [FromBody] EditarDonacionRequest donacion)
         {
             if (!ModelState.IsValid)
                 return BadRequest(donacion);
             var editarDonacion = accionesDonacion.Editar(donacion);
             return Ok(donacion);
-             if (!ModelState.IsValid) 
-                return BadRequest(donacion);
-            var editarDonacion = accionesDonacion.Editar(donacion);
-            return Ok(donacion);
+            
         }
 
         [HttpDelete("{id}")]
         public ActionResult BorrarDonacion(int id)
-        public ActionResult Borrar(int id)
         {
             var donacion = new BorrarDonacionRequest(id);
             accionesDonacion.Borrar(donacion);
