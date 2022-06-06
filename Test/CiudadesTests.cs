a21c0a8e8fb1b7b3e3684dfa6da0693314feb603
merienditas <maricersal@hotmail.com>
2022-06-06 13:21:40 +0200

    #39 CiudadesTest actualizado

--- Test/CiudadesTests.cs
+++ Test/CiudadesTests.cs
@@ -1,45 +1,88 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IESPeniasNegras.Ecotrans.Nucleo.Acciones.Ciudades;
+using IESPeniasNegras.Ecotrans.Nucleo.Model;
+using Xunit;
 
 namespace IESPeniasNegras.Ecotrans.Test;
 
 
-    public class CiudadesTests : IDisposable
+    public class CiudadesTests 
     {
 
-        private readonly DonacionesContext contexto;
-        public AccionesCiudades(DonacionesContext? donacionesContext = null)
+        private readonly DonacionesTestContext contexto;
+        private readonly AccionesCiudades accionesCiudades;
+
+        public  CiudadesTests()
+        {          
+            contexto = new DonacionesTestContext();    
+            accionesCiudades= new AccionesCiudades(contexto);
+        }
+      
+     ///////////////////////////TEST LISTAR//////////////////////////////////////////////////////////
+
+        [Fact]
+        public void Listar_Una_Ciudad()
         {
-            if(donacionesContext == null)
+            // Given
+            
+            var ciudad = new Ciudad()
             {
-                contexto = new DonacionesContext();
-            }
-            else
+                Nombre= "París"
+            };
+        
+            // When
+        
+            var peticion = new ListarCiudadRequest();
+            var respuesta = accionesCiudades.Listar(peticion);
+        
+            respuesta= accionesCiudades.Listar(peticion);
+
+            // Then
+            
+            Assert.NotEmpty(respuesta.Elementos);
+            Assert.Equal(respuesta.Elementos.First().Nombre, ciudad.Nombre);
+            
+        }
+
+     //////////////////////////TEST CREAR////////////////////////////////////////////////////////////
+
+        [Fact]
+        public void Crear_Una_Ciudad()
+        {
+            // Given
+            
+            var ciudad = new Ciudad()
             {
-                contexto = donacionesContext;
-            }
+                Nombre= "París"
+            };
+        
+
+            // When
+            var peticion= new CrearCiudadRequest();
+            var respuesta= accionesCiudades.Crear(peticion);
+
+            // Then
+            Assert.True(respuesta.Id>0);
+            Assert.Equal(respuesta.Nombre, ciudad.Nombre);
         }
 
-        public void Dispose()
-        {
-            contexto.Dispose();
-        }
+     [Fact]
 
-        public void Listar()
-        {
-        }
-        public void Crear()
-        { 
-        }
-
-
-        public void Editar()
+        public void Editar_Una_Ciudad_Existente()
         {
 
         }
 
-        public void Borrar()
+     [Fact]
+
+        public void Borrar_Una_Ciudad()
         {
+            
+    
 
         }
-    }
-
+    } 
