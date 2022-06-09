954fbc0f15a3c0283cb3f2844dfd4ccd8cf5b39c
merienditas <maricersal@hotmail.com>
2022-06-09 10:43:41 +0200

    #62 usuarios controller empezada con index vacio

--- FrontEnd/MVC/Controllers/UsuariosController.cs
+++ FrontEnd/MVC/Controllers/UsuariosController.cs
@@ -1,6 +1,20 @@
-﻿namespace MVC.Controllers
-{
-    public class UsuariosController
+﻿
+using IESPeniasNegras.Ecotrans.Nucleo.Acciones.Usuarios;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVC.Controllers;
+
+
+    public class UsuariosController : Controller
     {
+        public AccionesUsuarios accionesUsuarios { get; set; }
+
+        public IActionResult Index()
+        {
+            return View();
+        
+        }
     }
-}
+    
+
+
