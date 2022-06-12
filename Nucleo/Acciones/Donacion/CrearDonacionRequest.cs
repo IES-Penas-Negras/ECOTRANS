e79e9ebc38247defaa2f7f074603883a0acc407c
Carlosgv13 <crealmadrid618@gmail.com>
2022-06-12 17:26:42 +0200

    Cambios

--- Nucleo/Acciones/Donacion/CrearDonacionRequest.cs
+++ Nucleo/Acciones/Donacion/CrearDonacionRequest.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -8,19 +9,85 @@ using System.Threading.Tasks;
 namespace IESPeniasNegras.Ecotrans.Nucleo.Acciones.Donacion;
 
 
-    public class CrearDonacionRequest
+public class CrearDonacionRequest
+{
+    
+    public class Clases : DbContext
     {
-      [Required]
-      [StringLength(400)]
-      public string Titulo { get; set; }
-      public DateTime FechaInicio { get; set; }
-      public DateTime? FechaFin { get; set; }
-      [StringLength(3000)]
-      public string Descripcion { get; set; }
+       
+
+        //Definicion de las clases requeridas para Donacion
+        [Required]
+        [StringLength(400)]
+        public DbSet<Titulo> Titulos { get; set; }
+        public DateTime FechaInicio { get; set; }
+        public DateTime? FechaFin { get; set; }
+
+        //Definicion de la clase Descripcion que tiene un max de 3000 caracteres
+        [StringLength(3000)]
+        public string Descripcion { get; set; }
 
 
 
+        //protected override void OnModelCreating(ModelBuilder modelBuilder) => modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
 
 
     }
+
+
+
+
+
+
+    public class MenordeEdad : CrearDonacionRequest
+    {
+        private static int edadminima;
+
+        public MenordeEdad(string Apellidos, string Nombre)
+        { }
+
+        static MenordeEdad() => edadminima = 16;
+
+        //if(edadminima!=16)
+        //{
+        //    return(ErrorEventArgs);
+
+        //    }
+
+        
+     }
+
+   public class DonacionesDto
+    {
+
+        [Required]
+        [StringLength(400)]
+        public string Titulo { get; set; }
+        public DateTime FechaInicio { get; set; }
+        public DateTime? FechaFin { get; set; }
+
+
+        
+
+
+    }
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+    
 
