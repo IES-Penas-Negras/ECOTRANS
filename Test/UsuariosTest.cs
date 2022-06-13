097d030605eabc2ccb56cb4cb5295cb2b1703375
merienditas <maricersal@hotmail.com>
2022-06-13 13:34:22 +0200

    corregimos el UsuariosTest

--- Test/UsuariosTest.cs
+++ Test/UsuariosTest.cs
@@ -8,72 +8,41 @@ using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 
-namespace IESPeniasNegras.Ecotrans.TestUnitarios
-{
+namespace IESPeniasNegras.Ecotrans.TestUnitarios;
+
     public class UsuariosTest
     {
-        private readonly UsuariosTestContext contexto;
+        private readonly DonacionesTestContext contexto;
         private readonly AccionesUsuarios accionesUsuarios;
 
         public UsuariosTest()
         {
-            contexto = new UsuariosTestContext();
+            contexto = new DonacionesTestContext();
             accionesUsuarios = new AccionesUsuarios(contexto);
         }
-        [Fact]
-        public void TestDeUsuarios()
-        { 
-            var nombre = new Nombre()
-            {
-                Nombre = "Andrea"
-            };
-            var apellidos = new Apellidos()
-            {
-                Apellidos = "Bodnar"
-            };
-            var fechaDeNacimiento = new FechaDeNacimiento()
-            {
-                
-            };
-            
-            var dniNie = new DNI_NIE()
-            {
-                
-            };
-            var respuesta = accionesUsuarios.Crear(peticion);
 
-            Assert.True(respuesta.Nombre > 0);
-            Assert.Equal(respuesta.Apellidos, usuario.Apellidos);
-            Assert.Equal(respuesta.FechaDeNacimiento, usuario.FechaDeNacimiento);
-            Assert.Equal(respuesta.DNI_NIE, usuario.DNI_NIE);
-            
-        }
+        
         [Fact]
         public void ListarUsuarioExistente()
         {
-            var nombre = new Nombre()
+             /* var usuario = new Usuario()
             {
-                Nombre = "Andrea"
+                UserName = "Andrea",
+                Email = "prueba@dominio.es"            
             };
-            var apellidos = new Apellidos()
-            {
-                Apellidos = "Bodnar"
-            };
-            var fechaDeNacimiento = new FechaDeNacimiento()
-            {
-
-            };
-
-            var dniNie = new DNI_NIE()
-            {
-
-            };
+            */
+            var usuario = new Usuario(); 
+            usuario.UserName = "Andrea";
+            usuario.Email = "prueba@dominio.es";
+           
             contexto.Usuarios.Add(usuario);
             contexto.SaveChanges();
             
             var peticion = new ListarUsuarioRequest();
-            var respuesta = accionesUsuarioss.Listar(peticion);
+            var respuesta = accionesUsuarios.Listar(peticion);
+            
             //Entonces (T h e n)
             Assert.NotEmpty(respuesta.Elementos);
         }
-        [Fact]
+    } 
+       
